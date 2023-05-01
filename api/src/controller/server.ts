1a9b195ba1ee01667aa071ad185aaab6e6771d37
superdev0417 <happinessman0417@gmail.com>
2023-05-01 03:25:28 -0400

    final

--- api/src/controller/server.ts
+++ api/src/controller/server.ts
@@ -1,7 +1,8 @@
 
-import { RequestHandler, Request, Response  } from "express";
+import { Request, Response  } from "express";
 import {NFT} from "../model/basenft";
 import mongoose from "mongoose";
+import isEmpty from "../utils/isEmpty";
 
 export const getBaseNft = async (req: Request, res: Response ) => {
     try {
@@ -9,7 +10,7 @@ export const getBaseNft = async (req: Request, res: Response ) => {
         return res.status(200).json({code: 200, data: data, message: 'sucsess'});
     } catch (err) {
         console.log(err);
-        return res.status(500).json({code: 500, message: "failed"});
+        return res.status(500).json({code: 500, message: "server error!!!"});
     }
 }
 
@@ -17,82 +18,66 @@ export const getUserNft = async (req: Request, res: Response ) => {
     try {
 
         let { user_name } = req.body;
+
+        if (isEmpty(user_name)) {
+          return res.status(400).json({code: 400, message: "invalid request!!1"});
+        }
         let data = await NFT(user_name).find({}).sort({value: "desc"});
         return res.status(200).json({code: 200, data: data, message: 'sucsess'});
     } catch (err) {
         console.log(err);
-        return res.status(500).json({code: 500, message: "failed"});
+        return res.status(500).json({code: 500, message: "server error!!!"});
     }
 }
 
-export const createNft = async (req: Request, res: Response ) => {
-    
-    try {
-        let { data } = req.body;
-
-        let  nftData = JSON.parse(data);
-        
-        const nftschema = NFT('user_yash');
-
-        nftData.map(async (ele: any)  => {
-
-            let info = ele.info.split(" ");
-
-            let value = info.length > 5 ? Number(ele.value) + 5 * 5 + 10 * (info.length - 5) : Number(ele.value) + 5 * info.length;
-            
-            let userdata = new nftschema({
-                _id: new mongoose.Types.ObjectId,
-                name: ele.name,
-                image: ele.image,
-                value: value,
-                info: ele.info,
-            });
-
-            await userdata.save();
-    
-        })
-
-        return res.status(200).json({code: 200, message: "created succuess"});
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({code: 500, message: "failed"});
-    }
-}
-
-export const updateNft = async (req: Request, res: Response ) => {
+export const createAndUpdateNft = async (req: Request, res: Response ) => {
     
   try {
-    let { data } = req.body;
 
-    let  nftData = JSON.parse(data);
-    
-    const nftschema = NFT('user_yash');
-    
-    nftData.map(async (ele: any)  => {
+      let { _id, name, image, value, info, user_name} = req.body;
 
-      let info = ele.info.split(" ");
-
-      let value = info.length > 5 ? Number(ele.value) + 5 * 5 + 10 * (info.length - 5) : Number(ele.value) + 5 * info.length;
-      
-      let userdata = await nftschema.findById(new mongoose.Types.ObjectId(ele._id))
-
-      if (userdata) {
-          userdata.name = ele.name;
-          userdata.value = value;
-          userdata.info = ele.info;
-          userdata.image = ele.image;
-          await userdata.save()
-      } else {
-        return res.status(400).json({code: 400, message: "invalid request"});
+      if (isEmpty(_id) || isEmpty(name) || isEmpty(image) || isEmpty(info) || isEmpty(user_name) || isEmpty(value)) {
+        return res.status(400).json({code: 400, message: "invalid request!!1"});
       }
-      
-    })
 
-    return res.status(200).json({code: 200, message: "updated succuess"});
+      let nftschema = NFT(user_name);
+
+      let baseschema = NFT('base_nfts');
+
+      let infovalue = info.split(" ");      
+      
+      let userData = await nftschema.findById(new mongoose.Types.ObjectId(_id));
+      let existingData = await baseschema.findById(new mongoose.Types.ObjectId(_id));
+
+      if (isEmpty(userData) || isEmpty(existingData)) {
+        return res.status(400).json({code: 400, message: "invalid request!!1"});
+      }
+
+      value = info.length > 5 ? Number(existingData?.value) + 5 * 5 + 10 * (infovalue.length - 5) : Number(existingData?.value) + 5 * infovalue.length;
+
+      if (userData) {
+          userData.name = name;
+          userData.value = value;
+          userData.info = info;
+          userData.image = image;
+          await userData.save();
+          return res.status(202).json({code: 202, message: "updated NFT"});
+      } else {
+
+          let createdata = new nftschema({
+            _id: new mongoose.Types.ObjectId(_id),
+            name: name,
+            image: image,
+            value: value,
+            info: info,
+        });
+
+        await createdata.save();
+        return res.status(201).json({code: 201, message: "created NFT"});
+      }
 
   } catch (err) {
       console.log(err);
-      return res.status(500).json({code: 500, message: "failed"});
+      return res.status(500).json({code: 500, message: "server error!!!"});
   }
 }
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,14 +2,13 @@ import "express"
 import { app } from "../app"
 
 import { Router } from "express"
-import { getBaseNft, getUserNft, createNft, updateNft} from "../controller/server";
+import { getBaseNft, getUserNft,createAndUpdateNft} from "../controller/server";
 
 
 const router = Router();
 
 router.get('/basenft', getBaseNft);
 router.get('/usernft', getUserNft);
-router.post('/createnft', createNft);
-router.put('/updatenft', updateNft);
+router.post('/cunft', createAndUpdateNft);
 
 export default router;
