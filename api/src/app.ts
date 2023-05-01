b99f04cdbd314cadbe24bbef96e9632c18024679
superdev0417 <happinessman0417@gmail.com>
2023-04-30 22:34:22 -0400

    Revert "backend middle"
    This reverts commit 3688908c57eb1a6a421e9c29167d1c956fcd7d3c.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,21 +1,10 @@
-import express, {  Request, Response, NextFunction } from 'express';
-import bodyParser from 'body-parser';
+import express from 'express';
 import cors from 'cors';
-import routes from './routes';
-import {connector} from "./db/db";
 
 const app = express();
-
 app.use(cors())
+app.use('/', (req: express.Request, res: express.Response) => {
+    res.status(200).json({ message: "Hello World!" })
+});
 
-app.use(bodyParser.urlencoded({extended: true})); // Returns middleware that only parses urlencoded bodies. This parser accepts only UTF-8 encoding of the body and supports automatic inflation of gzip and deflate encodings.
-
-app.use(bodyParser.json()); // Returns middleware th
-
-connector();
-// app.use('/', (req: express.Request, res: express.Response) => {
-//     res.status(200).json({ message: "Hello World!" })
-// });
-
-app.use('/todos', routes);// This means all route path preceed this path
 export { app };
